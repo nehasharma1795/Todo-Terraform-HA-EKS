9dda059292575c3652cb831a6d365857dcc91fab
nehasharma1795 <155340024+nehasharma1795@users.noreply.github.com>
2025-05-01 23:35:19 +0530

    Create argocd-provider.tf

---
+++ agrocd/main/argocd-provider.tf
@@ -0,0 +1,20 @@
+# Fetch the EKS authentication token
+data "aws_eks_cluster_auth" "main" {
+  name = aws_eks_cluster.main.name
+}
+
+# Kubernetes provider to connect to EKS
+provider "kubernetes" {
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
+
+# Helm provider to use the same EKS connection
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.main.token
+  }
+}
