b95065807324cce8cb5124d4f8e33bcf07920238
nehasharma1795 <155340024+nehasharma1795@users.noreply.github.com>
2025-05-02 02:31:16 +0530

    Update ecr.tf

--- agrocd/ecr.tf
+++ agrocd/ecr.tf
@@ -1,76 +1,7 @@
-
-# ECR Backend Repository
 resource "aws_ecr_repository" "backend" {
-  name                 = var.ecr_backend_name
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  tags = {
-    Environment = var.environment
-    App         = var.ecr_backend_name
-  }
+  name = var.ecr_backend_name
 }
 
-# ECR Frontend Repository
 resource "aws_ecr_repository" "frontend" {
-  name                 = var.ecr_frontend_name
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  tags = {
-    Environment = var.environment
-    App         = var.ecr_frontend_name
-  }
-}
-
-# Backend Lifecycle Policy
-resource "aws_ecr_lifecycle_policy" "backend_policy" {
-  repository = aws_ecr_repository.backend.name
-
-  policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1
-        description  = "Expire untagged images older than 14 days"
-        selection = {
-          tagStatus   = "untagged"
-          countType   = "sinceImagePushed"
-          countUnit   = "days"
-          countNumber = 14
-        }
-        action = {
-          type = "expire"
-        }
-      }
-    ]
-  })
-}
-
-# Frontend Lifecycle Policy
-resource "aws_ecr_lifecycle_policy" "frontend_policy" {
-  repository = aws_ecr_repository.frontend.name
-
-  policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1
-        description  = "Expire untagged images older than 14 days"
-        selection = {
-          tagStatus   = "untagged"
-          countType   = "sinceImagePushed"
-          countUnit   = "days"
-          countNumber = 14
-        }
-        action = {
-          type = "expire"
-        }
-      }
-    ]
-  })
+  name = var.ecr_frontend_name
 }
