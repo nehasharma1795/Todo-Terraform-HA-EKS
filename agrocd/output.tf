95be9a379c7599d6fdd6137132e58ca159a32d26
nehasharma1795 <155340024+nehasharma1795@users.noreply.github.com>
2025-05-02 02:31:49 +0530

    Update output.tf

--- agrocd/output.tf
+++ agrocd/output.tf
@@ -1,28 +1,15 @@
-
-output "vpc_id" {
-  value = aws_vpc.main.id
+output "aws_account_id" {
+  value = data.aws_caller_identity.current.account_id
 }
 
-output "public_subnet_ids" {
-  value = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+output "aws_region" {
+  value = var.aws_region
 }
 
-output "private_subnet_ids" {
-  value = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+output "ecr_backend_name" {
+  value = aws_ecr_repository.backend.name
 }
 
-output "nat_gateway_id" {
-  value = aws_nat_gateway.nat.id
-}
-
-output "frontend_sg_id" {
-  value = aws_security_group.frontend_sg.id
-}
-
-output "backend_sg_id" {
-  value = aws_security_group.backend_sg.id
-}
-
-output "database_sg_id" {
-  value = aws_security_group.database_sg.id
+output "ecr_frontend_name" {
+  value = aws_ecr_repository.frontend.name
 }
